5784b7a48e0aec52b7acf58b477d0756fd70d38d
nickdos <nickdos@gmail.com>
2011-03-26 00:52:07 +0000

    Made default colour for WMS to be red instead of blue. JsDoc.
    git-svn-id: http://ala-hubs.googlecode.com/svn/trunk/hubs-webapp@181 1df052cb-f7c2-36cd-ec53-7bc1070de0c5

--- src/main/webapp/js/map.js
+++ src/main/webapp/js/map.js
@@ -497,8 +497,11 @@ function formatSciName(name, rankId) {
     return output;
 }
 
+/**
+ * Get a colour value (hex) by its index value in an array (31 elements)
+ */
 function getColourForIndex(index) {
-    var colours = ["3366CC","DC3912","FF9900","109618","990099","0099C6","DD4477","66AA00","B82E2E",
+    var colours = ["DC3912","3366CC","FF9900","109618","990099","0099C6","DD4477","66AA00","B82E2E",
         "316395","994499","22AA99","AAAA11","6633CC","E67200","8B0707","651067","329262","5574A6",
         "3B3EAC","B77322","16D620","B91383","F43595","9C5935","A9C413","2A778D","668D1C","BEA413",
         "0C5922","743411",""];
@@ -513,10 +516,13 @@ function getColourForIndex(index) {
     return hexCode;
 }
 
-function isInteger(value){
-  if((parseFloat(value) == parseInt(value)) && !isNaN(value)){
-      return true;
-  } else {
-      return false;
-  }
+/**
+ * Is input var an integer. Returns boolean.
+ */
+function isInteger(value) {
+    if ((parseFloat(value) == parseInt(value)) && !isNaN(value)) {
+        return true;
+    } else {
+        return false;
+    }
 }
