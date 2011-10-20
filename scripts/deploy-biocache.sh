bfa541cfe28e7411bdcc733bec1a23c18b9f076e
nickdos <nickdos@gmail.com>
2011-10-20 03:24:08 +0000

    Added ALA deploy script and added a sleep so that war file can explode before doing a cd.
    git-svn-id: http://ala-hubs.googlecode.com/svn/trunk/hubs-webapp@435 1df052cb-f7c2-36cd-ec53-7bc1070de0c5

---
+++ scripts/deploy-biocache.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# These steps should be ran manually and as super user
+echo "Downloading war file..."
+rm -Rf /usr/share/tomcat6/webapps/hubs*
+rm hubs-webapp-1.0-SNAPSHOT.war
+wget http://maven.ala.org.au/repository/au/org/ala/hubs-webapp/1.0-SNAPSHOT/hubs-webapp-1.0-SNAPSHOT.war
+echo "Copying war file to tomcat/webapps..."
+cp hubs-webapp-1.0-SNAPSHOT.war /usr/share/tomcat6/webapps/hubs-webapp.war
+sleep 2
+echo "Editting web.xml and restarting tomcat..."
+cd /usr/share/tomcat6/webapps/hubs-webapp/WEB-INF/
+cat web.xml  | sed 's/localhost:8080/biocache.ala.org.au/g' > web.xml.ala
+mv web.xml.ala web.xml
+
+/etc/init.d/tomcat6 restart
--- scripts/deploy-hubs.sh
+++ scripts/deploy-hubs.sh
@@ -7,6 +7,7 @@ rm hubs-webapp-1.0-SNAPSHOT.war
 wget http://maven.ala.org.au/repository/au/org/ala/hubs-webapp/1.0-SNAPSHOT/hubs-webapp-1.0-SNAPSHOT.war
 cp hubs-webapp-1.0-SNAPSHOT.war /usr/share/tomcat6/webapps/avh-demo.war
 cp hubs-webapp-1.0-SNAPSHOT.war /usr/share/tomcat6/webapps/ozcam-demo.war
+sleep 2 #
 mv /usr/share/tomcat6/webapps/ozcam-demo/WEB-INF/classes/hubs.properties.OZCAM /usr/share/tomcat6/webapps/ozcam-demo/WEB-INF/classes/hubs.properties
 mv /usr/share/tomcat6/webapps/avh-demo/WEB-INF/classes/hubs.properties.AVH /usr/share/tomcat6/webapps/avh-demo/WEB-INF/classes/hubs.properties
 
