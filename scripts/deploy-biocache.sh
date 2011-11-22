596f7a14384518128bcfda5b0a68523525a0ca57
nickdos <nickdos@gmail.com>
2011-11-22 00:35:21 +0000

    Added check for if webapp directory exists, before attempting sed and restart.
    git-svn-id: http://ala-hubs.googlecode.com/svn/trunk/hubs-webapp@458 1df052cb-f7c2-36cd-ec53-7bc1070de0c5

--- scripts/deploy-biocache.sh
+++ scripts/deploy-biocache.sh
@@ -6,10 +6,19 @@ rm hubs-webapp-1.0-SNAPSHOT.war
 wget http://maven.ala.org.au/repository/au/org/ala/hubs-webapp/1.0-SNAPSHOT/hubs-webapp-1.0-SNAPSHOT.war
 echo "Copying war file to tomcat/webapps..."
 cp hubs-webapp-1.0-SNAPSHOT.war /usr/share/tomcat6/webapps/hubs-webapp.war
-sleep 2
+#sleep 5
+webappExploded=false
+while [ ! $webappExploded ]; 
+do
+    sleep 2
+    if [  -d "/usr/share/tomcat6/webapps/hubs-webapp/WEB-INF/" ]; then
+       webappExploded=true
+    fi
+done
 echo "Editting web.xml and restarting tomcat..."
 cd /usr/share/tomcat6/webapps/hubs-webapp/WEB-INF/
 cat web.xml  | sed 's/localhost:8080/biocache.ala.org.au/g' > web.xml.ala
 mv web.xml.ala web.xml
 
 /etc/init.d/tomcat6 restart
+
