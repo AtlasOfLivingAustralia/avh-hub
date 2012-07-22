2083972e226404cc678d7f9db46c6c3942814f91
djtfmartin <djtfmartin@gmail.com>
2012-07-22 04:29:06 +0000

    fix for deploy script
    git-svn-id: http://ala-hubs.googlecode.com/svn/trunk/hubs-webapp@803 1df052cb-f7c2-36cd-ec53-7bc1070de0c5

--- scripts/deploy-hubs.sh
+++ scripts/deploy-hubs.sh
@@ -5,18 +5,18 @@ rm -Rf /usr/share/tomcat6/webapps/ozcam*
 rm -Rf /usr/share/tomcat6/webapps/avh*
 rm hubs-webapp-1.0-SNAPSHOT.war
 wget http://maven.ala.org.au/repository/au/org/ala/hubs-webapp/1.0-SNAPSHOT/hubs-webapp-1.0-SNAPSHOT.war
-cp hubs-webapp-1.0-SNAPSHOT.war /usr/share/tomcat6/webapps/avh-demo.war
+cp hubs-webapp-1.0-SNAPSHOT.war /usr/share/tomcat6/webapps/avh.war
 cp hubs-webapp-1.0-SNAPSHOT.war /usr/share/tomcat6/webapps/ozcam-demo.war
 sleep 2 #
 mv /usr/share/tomcat6/webapps/ozcam-demo/WEB-INF/classes/hubs.properties.OZCAM /usr/share/tomcat6/webapps/ozcam-demo/WEB-INF/classes/hubs.properties
-mv /usr/share/tomcat6/webapps/avh-demo/WEB-INF/classes/hubs.properties.AVH /usr/share/tomcat6/webapps/avh-demo/WEB-INF/classes/hubs.properties
+mv /usr/share/tomcat6/webapps/avh-demo/WEB-INF/classes/hubs.properties.AVH /usr/share/tomcat6/webapps/avh/WEB-INF/classes/hubs.properties
 
 cd /usr/share/tomcat6/webapps/ozcam-demo/WEB-INF/
 sed  's/>ala</>ozcam</g' < web.xml  | sed 's/localhost:8080/ozcam-demo.ala.org.au/g' > web.xml.ozcam
 mv web.xml.ozcam web.xml
 
-cd /usr/share/tomcat6/webapps/avh-demo/WEB-INF/
-sed  's/>ala</>avh</g' < web.xml  | sed 's/localhost:8080/avh-demo.ala.org.au/g' > web.xml.avh
+cd /usr/share/tomcat6/webapps/avh/WEB-INF/
+sed  's/>ala</>avh</g' < web.xml  | sed 's/localhost:8080/avh.ala.org.au/g' > web.xml.avh
 mv web.xml.avh web.xml
 
 
