ba2ac4992288522297b6d341d4a0d7f99d0321e5
nickdos <nickdos@gmail.com>
2012-12-19 23:29:47 +0000

    Fixed bug where fq param value was being split into 2 fq params due to semi colon character.
    git-svn-id: http://ala-hubs.googlecode.com/svn/trunk/hubs-webapp@957 1df052cb-f7c2-36cd-ec53-7bc1070de0c5

--- src/main/java/org/ala/hubs/util/CustomStringToArrayConverter.java
+++ src/main/java/org/ala/hubs/util/CustomStringToArrayConverter.java
@@ -6,6 +6,6 @@ import org.springframework.util.StringUtils;
 public class CustomStringToArrayConverter implements Converter<String, String[]> {
     @Override
     public String[] convert(String source) {
-        return StringUtils.delimitedListToStringArray(source, ";");
+        return StringUtils.delimitedListToStringArray(source, "||");
     }
 }
