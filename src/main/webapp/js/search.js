9370542bcc9486e62aeef5038108db274cef7c92
nickdos <nickdos@gmail.com>
2013-07-10 00:58:22 +0000

    Fixed issue #92 - clearing filters with spaces encoded as "+".
    git-svn-id: http://ala-hubs.googlecode.com/svn/trunk/hubs-webapp@1091 1df052cb-f7c2-36cd-ec53-7bc1070de0c5

--- src/main/webapp/js/search.js
+++ src/main/webapp/js/search.js
@@ -632,7 +632,7 @@ function removeFacet(el) {
     if (fqList instanceof Array) {
         //alert("fqList is an array");
         for (var i in fqList) {
-            var thisFq = decodeURIComponent(fqList[i]); //.replace(':[',':'); // for dates to work
+            var thisFq = decodeURIComponent(fqList[i].replace('+',' ')); //.replace(':[',':'); // for dates to work
             //alert("fq = "+thisFq + " || facet = "+decodeURIComponent(facet));
             if (thisFq.indexOf(decodeURIComponent(facet)) != -1) {  // if(str1.indexOf(str2) != -1){
                 //alert("removing fq: "+fqList[i]);
