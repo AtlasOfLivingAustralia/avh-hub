59b685270049db64d6692c4556b5fe809f247c21
nickdos <nickdos@gmail.com>
2014-02-07 01:10:47 +0000

    Added taglib to allow generic skin header to be loaded from an external URL or an internal  JSP file.
    git-svn-id: http://ala-hubs.googlecode.com/svn/trunk/hubs-webapp@1162 1df052cb-f7c2-36cd-ec53-7bc1070de0c5

---
+++ src/main/java/org/ala/hubs/util/GenericSkinTag.java
@@ -0,0 +1,116 @@
+/* *************************************************************************
+ *  Copyright (C) 2014 Atlas of Living Australia
+ *  All Rights Reserved.
+ * 
+ *  The contents of this file are subject to the Mozilla Public
+ *  License Version 1.1 (the "License"); you may not use this file
+ *  except in compliance with the License. You may obtain a copy of
+ *  the License at http://www.mozilla.org/MPL/
+ * 
+ *  Software distributed under the License is distributed on an "AS
+ *  IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ *  implied. See the License for the specific language governing
+ *  rights and limitations under the License.
+ ***************************************************************************/
+package org.ala.hubs.util;
+
+import org.ala.hubs.service.WebService;
+import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.tags.RequestContextAwareTag;
+
+import javax.inject.Inject;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletResponseWrapper;
+import java.io.*;
+
+
+/**
+ * Taglib for inserting header/footer from external URL
+ *
+ * @author "Nick dos Remedios <Nick.dosRemedios@csiro.au>"
+ */
+public class GenericSkinTag extends RequestContextAwareTag {
+    private final static Logger logger = Logger.getLogger(GenericSkinTag.class);
+    @Inject
+    private ServletContext servletContext;
+    @Inject
+    private WebService apacheHttpWebService;
+    @Value("${site.headerUrl:/WEB-INF/genericSkin/header.jsp}")
+    private String headerUrl = null;
+    @Value("${site.displayName:Generic Skin}")
+    private String displayName = null;
+
+    @Override
+    protected int doStartTagInternal() throws Exception {
+        /*
+         * Servlet Tags are instantiated by the Servlet and NOT by Spring but the
+         * RequestContextAwareTag provides access to the request context, which then allows
+         * us to autowire this class manually. In this case the implementation
+         * is provided which uses EhCache to provide caching of the bean (5 minutes before it
+         * then goes to disk to see if there has been a change).
+         */
+        if (apacheHttpWebService == null) {
+            logger.debug("Autowiring the bean");
+            WebApplicationContext wac = getRequestContext().getWebApplicationContext();
+            AutowireCapableBeanFactory acbf = wac.getAutowireCapableBeanFactory();
+            acbf.autowireBean(this);
+        }
+
+        String headerHtml = "";
+
+        if (StringUtils.startsWith(headerUrl, "http")) {
+            // external HTTP url
+            headerHtml = apacheHttpWebService.getForUrl(headerUrl); // cached by EhCache
+        } else if (StringUtils.isNotBlank(headerUrl)) {
+            // internal file
+            InputStream inputStream = null;
+            try {
+                inputStream = servletContext.getResourceAsStream(headerUrl);
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                headerHtml = org.apache.commons.io.IOUtils.toString(bufferedReader); //bufferedReader.readLine();
+            } catch (Exception ex) {
+                logger.error("Error reading header: " + ex.getLocalizedMessage(), ex);
+            } finally {
+                if (inputStream != null) {
+                    inputStream.close();
+                }
+            }
+        }
+
+        if (StringUtils.isNotBlank(headerHtml)) {
+            // JSTL style substitution
+            headerHtml = StringUtils.replace(headerHtml, "${orgNameLong}", displayName);
+            headerHtml = StringUtils.replace(headerHtml, "class=\"container\"", "class=\"container-OFF\""); // CSS fix for full width
+            pageContext.getOut().print(headerHtml);
+        }
+
+        return SKIP_BODY;
+    }
+
+    private class CharArrayWriterResponse extends HttpServletResponseWrapper {
+
+        private final CharArrayWriter charArray = new CharArrayWriter();
+
+        public CharArrayWriterResponse(HttpServletResponse response) {
+            super(response);
+        }
+
+        @Override
+        public PrintWriter getWriter() throws IOException {
+            return new PrintWriter(charArray);
+        }
+
+        public String getOutput() {
+            return charArray.toString();
+        }
+    }
+
+}
+
+
