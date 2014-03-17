477238ec7da7d9c18136c3f81b72bf32762cb6cc
nickdos <nickdos@gmail.com>
2014-03-17 06:22:44 +0000

    Cached resources plugin added.
    git-svn-id: http://ala-hubs.googlecode.com/svn/trunk/avh-biocache-hub@1255 1df052cb-f7c2-36cd-ec53-7bc1070de0c5

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -60,12 +60,14 @@ grails.project.dependency.resolution = {
         // plugins for the compile step
         //compile ":scaffolding:2.0.1"
         compile ':cache:1.1.1'
+        compile ":cache-headers:1.1.6"
 
         // plugins needed at runtime but not for compilation
         //runtime ":hibernate:3.6.10.7" // or ":hibernate4:4.1.11.6"
         //runtime ":database-migration:1.3.8"
         //runtime ":jquery:1.10.2.2"
         runtime ":resources:1.2.1"
+        runtime ":cached-resources:1.0"
         // Uncomment these (or add new ones) to enable additional resources capabilities
         //runtime ":zipped-resources:1.0.1"
         //runtime ":cached-resources:1.1"
