a902682c510e42c28cfb8ebdc9fa56ae60e43f32
nickdos <nickdos@gmail.com>
2014-03-19 03:27:47 +0000

    Various minor changes.
    git-svn-id: http://ala-hubs.googlecode.com/svn/trunk/avh-biocache-hub@1274 1df052cb-f7c2-36cd-ec53-7bc1070de0c5

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -67,7 +67,8 @@ grails.project.dependency.resolution = {
         //runtime ":database-migration:1.3.8"
         //runtime ":jquery:1.10.2.2"
         runtime ":resources:1.2.1"
-        runtime ":cached-resources:1.0"
+        runtime ":cached-resources:1.1"
+        runtime ":zipped-resources:1.0.1"
         // Uncomment these (or add new ones) to enable additional resources capabilities
         //runtime ":zipped-resources:1.0.1"
         //runtime ":cached-resources:1.1"
