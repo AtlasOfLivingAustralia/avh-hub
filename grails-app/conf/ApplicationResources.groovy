059e1c0e447e3bc4e43a405d0924edc81ad4aedf
nickdos <nickdos@gmail.com>
2014-04-09 02:16:44 +0000

    Fixed  renamed resource bundle.
    git-svn-id: http://ala-hubs.googlecode.com/svn/trunk/avh-biocache-hub@1341 1df052cb-f7c2-36cd-ec53-7bc1070de0c5

--- grails-app/conf/ApplicationResources.groovy
+++ grails-app/conf/ApplicationResources.groovy
@@ -6,7 +6,7 @@ modules = {
     // Define your skin module here - it must 'dependsOn' either bootstrap (ALA version) or bootstrap2 (unmodified) and core
 
     avh {
-        dependsOn 'bootstrap', 'core' // from ala-web-theme plugin
+        dependsOn 'bootstrap', 'hubCore' // from ala-web-theme plugin
         resource url:[dir:'css', file:'avh/style.css']
     }
 
