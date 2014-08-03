b311bb57469e97f5005f0b089cd67840e456608f
nickdos <nickdos@gmail.com>
2014-08-04 09:31:47 +1000

    Fix for log4j prod log file location

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -171,6 +171,8 @@ environments {
     }
 }
 
+logging.dir = (System.getProperty('catalina.base') ? System.getProperty('catalina.base') + '/logs'  : '/var/log/tomcat6')
+
 // log4j configuration
 log4j = {
     // Example of changing the log pattern for the default console appender:
@@ -182,7 +184,7 @@ log4j = {
     appenders {
         environments {
             production {
-                rollingFile name: "tomcatLog", maxFileSize: 102400000, file: "/var/log/tomcat6/avh-hub.log", threshold: org.apache.log4j.Level.ERROR, layout: pattern(conversionPattern: "%d %-5p [%c{1}] %m%n")
+                rollingFile name: "tomcatLog", maxFileSize: 102400000, file: logging.dir + "/avh-hub.log", threshold: org.apache.log4j.Level.ERROR, layout: pattern(conversionPattern: "%d %-5p [%c{1}] %m%n")
                 //'null' name: "stacktrace"
                 console name: "stdout", layout: pattern(conversionPattern: "%d %-5p [%c{1}]  %m%n"), threshold: org.apache.log4j.Level.WARN
             }
