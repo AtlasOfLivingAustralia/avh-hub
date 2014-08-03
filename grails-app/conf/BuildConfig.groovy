40c96cc1619bded6f3726782e1ff87ca7ba5921a
nickdos <nickdos@gmail.com>
2014-08-04 09:37:26 +1000

    Changed plugin dep. versions to static values
    From number ranges.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -77,7 +77,7 @@ grails.project.dependency.resolution = {
         //runtime ":zipped-resources:1.0.1"
         //runtime ":cached-resources:1.1"
         //runtime ":yui-minify-resources:0.1.5"
-        runtime ":biocache-hubs:[0.1,]"
-        runtime ":ala-web-theme:[0.1,]"
+        runtime ":biocache-hubs:0.46"
+        runtime ":ala-web-theme:0.8.1"
     }
 }
