600ee082b24d293e1626fd0fe879485b96f584be
Dave Martin <djtfmartin@users.noreply.github.com>
2014-10-22 14:39:47 +1100

    bump biocache-hub plugin to 0.60
    bump biocache-hub plugin to 0.60

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -64,7 +64,7 @@ grails.project.dependency.resolution = {
         compile ":cache-headers:1.1.6"
         runtime ":resources:1.2.8"
         runtime ":cached-resources:1.0"
-        runtime ":biocache-hubs:0.57"
+        runtime ":biocache-hubs:0.60"
         runtime ":ala-web-theme:0.8.1"
     }
 }
