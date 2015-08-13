ed58fc1084637cc0c439314b15018a059ed7e0c1
nickdos <nickdos@gmail.com>
2015-08-13 13:48:19 +1000

    Update ala-auth version

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -55,6 +55,6 @@ grails.project.dependency.resolution = {
         runtime ":resources:1.2.8"
         runtime ":cached-resources:1.0"
         runtime ":biocache-hubs:0.72-SNAPSHOT"
-        runtime ":ala-auth:1.1"
+        runtime ":ala-auth:1.3.1"
     }
 }
