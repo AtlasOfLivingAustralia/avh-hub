a23e8df4feb3b470bfb322afcd2a1ec8a217bd8f
Nick dos Remedios <nickdos@gmail.com>
2016-03-21 15:07:25 +1100

    Update biocache-hubs to 0.75.2
    Hotfix for AVH pagination bug

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -54,7 +54,7 @@ grails.project.dependency.resolution = {
         runtime ":jquery:1.11.1"
         runtime ":resources:1.2.8"
         runtime ":cached-resources:1.0"
-        runtime ":biocache-hubs:0.75"
+        runtime ":biocache-hubs:0.75.2"
         runtime ":ala-auth:1.3.1"
     }
 }
