ea379f09f801674161ab96411b66b5fb79abb0a9
Peter Ansell <p_ansell@yahoo.com>
2016-09-20 12:29:05 +1000

    Update to releases for biocache-hubs and downloads-plugin

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -54,8 +54,8 @@ grails.project.dependency.resolution = {
         runtime ":jquery:1.11.1"
         runtime ":resources:1.2.8"
         runtime ":cached-resources:1.0"
-        runtime ":biocache-hubs:1.0-SNAPSHOT"
-        runtime ":downloads-plugin:1.1-SNAPSHOT"
+        runtime ":biocache-hubs:1.1"
+        runtime ":downloads-plugin:1.1"
         runtime ":ala-auth:1.3.1"
     }
 }
