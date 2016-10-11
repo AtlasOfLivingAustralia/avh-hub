c4b29b981f3c9b0a10fa402255fb979537ee8268
Peter Ansell <p_ansell@yahoo.com>
2016-10-11 14:54:33 +1100

    Add ala-bootstrap2 plugin

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -56,6 +56,7 @@ grails.project.dependency.resolution = {
         runtime ":cached-resources:1.0"
         runtime ":ala-auth:1.3.4"
         runtime ":biocache-hubs:1.2-SNAPSHOT"
+        runtime ":ala-bootstrap2:2.4.3-SNAPSHOT"
         runtime ":downloads-plugin:1.1"
     }
 }
