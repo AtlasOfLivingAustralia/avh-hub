bbad8c73f184acb61e6e717780476bd2e3c25bb9
Peter Ansell <p_ansell@yahoo.com>
2016-10-28 12:05:28 +1100

    Start testing again with biocache-hubs:1.3-SNAPSHOT
    Did not notice that someone released biocache-hubs:1.2 a few days ago, with SNAPSHOT dependencies still intact, so starting testing all over again

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -55,7 +55,7 @@ grails.project.dependency.resolution = {
         runtime ":resources:1.2.8"
         runtime ":cached-resources:1.0"
         runtime ":ala-auth:1.3.4"
-        runtime ":biocache-hubs:1.2-SNAPSHOT"
+        runtime ":biocache-hubs:1.3-SNAPSHOT"
         runtime ":ala-bootstrap2:2.4.3-SNAPSHOT"
         runtime ":downloads-plugin:1.1"
     }
