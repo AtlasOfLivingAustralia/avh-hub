ee0b215c138a62be8561d4b39a752eb30d5f42a0
nickdos <nickdos@gmail.com>
2018-03-22 11:33:17 +1100

    Logback fixes

--- grails-app/conf/logback.groovy
+++ grails-app/conf/logback.groovy
@@ -1,21 +1,17 @@
 import ch.qos.logback.core.util.FileSize
-import grails.util.BuildSettings
 import grails.util.Environment
 import org.springframework.boot.logging.logback.ColorConverter
 import org.springframework.boot.logging.logback.WhitespaceThrowableProxyConverter
 
-import java.nio.charset.Charset
-
 conversionRule 'clr', ColorConverter
 conversionRule 'wex', WhitespaceThrowableProxyConverter
 
-// See http://logback.qos.ch/manual/groovy.html for details on configuration
 def loggingDir = (System.getProperty('catalina.base') ? System.getProperty('catalina.base') + '/logs' : './logs')
-def appName = 'ala-hub'
-def TOMCAT_LOG = 'TOMCAT_LOG'
+def appName = 'avh-hub'
+final APPENDER = 'APP_APPENDER'
 switch (Environment.current) {
     case Environment.PRODUCTION:
-        appender(TOMCAT_LOG, RollingFileAppender) {
+        appender(APPENDER, RollingFileAppender) {
             file = "$loggingDir/${appName}.log"
             encoder(PatternLayoutEncoder) {
                 pattern =
@@ -26,7 +22,7 @@ switch (Environment.current) {
                                 '%m%n%wex' // Message
             }
             rollingPolicy(FixedWindowRollingPolicy) {
-                fileNamePattern = "$loggingDir/$appName.%i.log.gz"
+                fileNamePattern = "$loggingDir/${appName}.%i.log.gz"
                 minIndex=1
                 maxIndex=4
             }
@@ -35,40 +31,66 @@ switch (Environment.current) {
             }
         }
         break
-    default:
-        appender(TOMCAT_LOG, ConsoleAppender) {
+    case Environment.TEST:
+        appender(APPENDER, RollingFileAppender) {
+            file = "$loggingDir/${appName}.log"
             encoder(PatternLayoutEncoder) {
-                charset = Charset.forName('UTF-8')
-
                 pattern =
-                        '%clr(%d{yyyy-MM-dd HH:mm:ss.SSS}){faint} ' + // Date
-                                '%clr(%5p) ' + // Log level
-                                '%clr(---){faint} %clr([%15.15t]){faint} ' + // Thread
-                                '%clr(%-40.40logger{39}){cyan} %clr(:){faint} ' + // Logger
+                        '%d{yyyy-MM-dd HH:mm:ss.SSS} ' + // Date
+                                '%5p ' + // Log level
+                                '--- [%15.15t] ' + // Thread
+                                '%-40.40logger{39} : ' + // Logger
                                 '%m%n%wex' // Message
+            }
+            rollingPolicy(FixedWindowRollingPolicy) {
+                fileNamePattern = "$loggingDir/${appName}.%i.log.gz"
+                minIndex=1
+                maxIndex=4
+            }
+            triggeringPolicy(SizeBasedTriggeringPolicy) {
+                maxFileSize = FileSize.valueOf('1MB')
+            }
+        }
+        break
+    case Environment.DEVELOPMENT:
+        [
+                (DEBUG): [ // DEBUG and TRACE should only be enabled for non-production environments
+//                           'grails.app',
+                           'au.org.ala.cas',
+                           'au.org.ala.hub',
+                           'au.org.ala.bootstrap3',
+                           'au.org.ala.biocache.hubs',
+                           'au.org.ala.biocache.hubs.avh',
+                           'au.org.ala.downloads',
+                           'au.org.ala.downloads.plugin',
+                           'grails.app',
+                           'org.grails.plugins',
+                           'grails.plugin.cache'
+                ],
+                (TRACE): [
+                ]
+        ].each { level, names -> names.each { name -> logger(name, level) } }
+
+    default:
+        appender(APPENDER, ConsoleAppender) {
+            encoder(PatternLayoutEncoder) {
+                pattern = '%clr(%d{yyyy-MM-dd HH:mm:ss.SSS}){faint} ' + // Date
+                        '%clr(%5p) ' + // Log level
+                        '%clr(---){faint} %clr([%15.15t]){faint} ' + // Thread
+                        '%clr(%-40.40logger{39}){cyan} %clr(:){faint} ' + // Logger
+                        '%m%n%wex' // Message
             }
         }
         break
 }
 
-def targetDir = BuildSettings.TARGET_DIR
-if (Environment.isDevelopmentMode() && targetDir != null) {
-    appender("FULL_STACKTRACE", FileAppender) {
-        file = "${targetDir}/stacktrace.log"
-        append = true
-        encoder(PatternLayoutEncoder) {
-            pattern = "%level %logger - %msg%n"
-        }
-    }
-    logger("StackTrace", ERROR, ['FULL_STACKTRACE'], false)
-}
-jmxConfigurator()
 
-root(DEBUG, [TOMCAT_LOG])
+
+root(WARN, [APPENDER])
+
 [
         (OFF): [],
         (ERROR): [
-                'au.org.ala.cas.client',
                 'grails.spring.BeanBuilder',
                 'grails.plugin.webxml',
                 'grails.plugin.cache.web.filter',
@@ -81,10 +103,9 @@ root(DEBUG, [TOMCAT_LOG])
         ],
         (INFO): [
                 'grails.plugin.externalconfig.ExternalConfig',
-                'grails.app',
+                'au.org.ala'
         ],
-        (DEBUG): [
-                'grails.app','au.org.ala.cas','au.org.ala.biocache', 'grails.plugin.cache'
+        (DEBUG): [ // DEBUG and TRACE should only be enabled for non-production environments
         ],
         (TRACE): [
         ]
