a49bf866e1ca5db5b881dd64566d4b6a4249846f
nickdos <nickdos@gmail.com>
2018-07-06 16:10:09 +1000

    Version 0.8.5
    Updated biocach-hubs to 1.4.10

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -54,7 +54,7 @@ grails.project.dependency.resolution = {
         runtime ":jquery:1.11.1"
         runtime ":resources:1.2.8"
         runtime ":cached-resources:1.0"
-        runtime ":biocache-hubs:1.4.7"
+        runtime ":biocache-hubs:1.4.10"
         runtime ":downloads-plugin:1.3.3"
         runtime ":ala-auth:1.3.4"
     }
